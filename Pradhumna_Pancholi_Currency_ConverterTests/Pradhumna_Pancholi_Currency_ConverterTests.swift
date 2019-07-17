5c3f7aeaa1f5512342fa52e9290f924371eee184
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-07-17 15:22:36 -0400

    Initial Commit

---
+++ Pradhumna_Pancholi_Currency_ConverterTests/Pradhumna_Pancholi_Currency_ConverterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pradhumna_Pancholi_Currency_ConverterTests.swift
+//  Pradhumna_Pancholi_Currency_ConverterTests
+//
+//  Created by Pradhumna Pancholi on 2019-07-17.
+//  Copyright © 2019 Pradhumna Pancholi. All rights reserved.
+//
+
+import XCTest
+@testable import Pradhumna_Pancholi_Currency_Converter
+
+class Pradhumna_Pancholi_Currency_ConverterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
